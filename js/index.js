b278e5f0855add7d9c100f2b16510eda2504d59b
Nabeel <nabeel@qburst.com>
2015-01-30 13:54:48 +0530

    mail chimp integration

--- js/index.js
+++ js/index.js
@@ -8,32 +8,50 @@ $('#down-arrow').on('click', function(e){
 });
 function validate(clicked_id)
 {
-    var e = $(clicked_id);
-    var form = e.parent('form').get(0);
-	var user_id =form.id;
-	var check = document.getElementById(user_id).checkValidity();
+	var check = document.getElementById(clicked_id.id).checkValidity();
 	var errorboxid;
-	if (clicked_id.id == "button-ok" ) {
+	
+	if (clicked_id.id == "email_validation" ) {
 		errorboxid = "text_wrap";
 	}
 	else{
 		errorboxid = "text_wrap1";
 	}
-    
     if (check == false) {
+    	$(".text").html("enter valid email");
 		$('#'+errorboxid).show().delay(2000).fadeOut();
+		return false;
        
 	}
-	else{
-		if (errorboxid=="text_wrap") {
-       	  $('#section-1content').hide();
-		  $('#section-1contentx').show();
-        }
-        else{
-       	  $('#section-4main').hide();
-		  $('#section-4mainx').show();
-       }
-   }
-
+	var form = $('#email_validation');
+	$.ajax({
+	       type: 'GET',
+	       url: '//us10.list-manage.com/subscribe/post-json?u=6e86c51ea42cc062099af08b0&id=3369562b9b&c=?',
+	       data: form.serialize(),
+	       cache : false,
+	       dataType : 'json',
+	       error: function(result) {
+	       	debugger;
+	       },
+	       success : function(result) {
+	       	console.log(result);
+	       	var res = result.result;
+	       	if (res == "error") {$(".text").html("already subscribed");$('#'+errorboxid).show().delay(2000).fadeOut();}
+	       	else {	
+	       	if (errorboxid=="text_wrap") {
+				$('#section-1content').hide();
+				$('#section-1contentx').show();
+			}
+			else{
+				$('#section-4main').hide();
+				$('#section-4mainx').show();
+			}
+		    } 
+	       },
+	       complete : function() { 
+	       	debugger;
+			
+		}
+	});
 }
 
